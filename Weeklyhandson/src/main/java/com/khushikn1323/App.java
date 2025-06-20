0bbe96e0e08c466397833c1fb12cf07a849c19ff
khushikn1323 <khushikn7@gmail.com>
2025-06-20 19:06:10 +0530

    first commit

---
+++ Weeklyhandson/src/main/java/com/khushikn1323/App.java
@@ -0,0 +1,13 @@
+package com.khushikn1323;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
